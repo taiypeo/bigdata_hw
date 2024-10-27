4b9c688f6e4c1c5f731031850465e21834744d94
taiypeo <Qwertygid@gmail.com>
2024-10-27 15:25:49 +0300

    Save

--- hw03/setup_hive.sh
+++ hw03/setup_hive.sh
@@ -4,11 +4,10 @@ usage () {
     echo "Usage:"
     echo "./setup_hive.sh \\"
     echo "  --namenode <namenode_ip>"
-    echo "  --hadoop-password <hadoop_user_password>"
     echo "  --hive-password <hive_postgres_password>"
 }
 
-VALID_ARGS=$(getopt -o '' --long help,namenode:,hadoop-password:,hive-password: -- "$@")
+VALID_ARGS=$(getopt -o '' --long help,namenode:,hive-password: -- "$@")
 if [[ $? -ne 0 ]] && usage; then
     exit 1
 fi
@@ -22,10 +21,6 @@ while true; do
         ;;
     --namenode)
         NAMENODE_HOST="$2"
-        shift 2
-        ;;
-    --hadoop-password)
-        HADOOP_PASSWORD="$2"
         shift 2
         ;;
     --hive-password)
@@ -43,42 +38,14 @@ if [[ -z $NAMENODE_HOST ]]; then
     usage
     exit 1
 fi
-if [[ -z $HADOOP_PASSWORD ]]; then
-    echo "No hadoop user password provided!"
-    usage
-    exit 1
-fi
 if [[ -z $HIVE_POSTGRES_PASSWORD ]]; then
     echo "No hive postgres password provided!"
     usage
     exit 1
 fi
 
-echo "Updating the system and installing Java 11"
-yes | sudo apt-get update
-yes | sudo apt-get upgrade
-yes | sudo apt-get install openjdk-11-jre
-yes | sudo apt-get install openjdk-11-jdk-headless
-
-echo "Creating the hadoop user"
-(
-    sudo useradd -m hadoop -p "$(echo ${HADOOP_PASSWORD} | openssl passwd -1 -stdin)" && \
-    sudo chsh -s /bin/bash hadoop
-) || echo "User already exists! Skipping"
-(
-    sudo mkdir -p "/home/hadoop/.ssh" && \
-    sudo chown hadoop:hadoop "/home/hadoop/.ssh" && \
-    sudo cp "$HOME/.ssh/authorized_keys" "/home/hadoop/.ssh/authorized_keys" && \
-    sudo chown hadoop:hadoop "/home/hadoop/.ssh/authorized_keys"
-) || echo "File does not exist! Skipping"
-
 sudo -i -u hadoop bash << OUTEREOF
 cd
-echo "Generating the hadoop user ssh keys"
-yes | ssh-keygen -q -t ed25519 -f "\$HOME/.ssh/host_key" -N ""
-chmod 600 "\$HOME/.ssh/host_key"
-chmod 600 "\$HOME/.ssh/host_key.pub"
-
 if [[ ! -f "apache-hive-4.0.1-bin.tar.gz" ]]; then
     echo "Downloading Apache Hive"
     wget -q "https://dlcdn.apache.org/hive/hive-4.0.1/apache-hive-4.0.1-bin.tar.gz"
